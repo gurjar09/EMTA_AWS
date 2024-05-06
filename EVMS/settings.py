95d6ab7de1682f9d9c146488e8c41e9889c6a6f1
sagar gurjar <sagarchhalotre@gmail.com>
2024-05-06 11:47:27 +0530

    host commit

--- EVMS/settings.py
+++ EVMS/settings.py
@@ -138,9 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = '/path/to/your/static/files/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
